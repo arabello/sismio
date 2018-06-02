1dee350920dad391e356403025e7ad7c84a31b33
albertobagnacani <albertobagnacani@gmail.com>
2018-06-02 16:01:54 +0200

    Add TODO in Logger

--- Sismio/Sismio/Program.cs
+++ Sismio/Sismio/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
-using Logger = io.sismio.log.Logger;
+using Sismio.io.sismio.log;
+using Sismio.io.sismio.utente;
 
 namespace Sismio
 {
@@ -12,10 +13,13 @@ namespace Sismio
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            Logger.Log();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            Amministratore pippo = new Amministratore("Alberto", "Bagnacani", "albertobagnacani@gmail.com", "Pippo", "24i1h4i1giu12g2i1is", "ooieowieowi9239", true);
+            Logger.Scrivi(pippo, "aggiunto nuovo utente: Pluto");
+            Logger.Scrivi(pippo, "abilitato login remoto a: Pluto");
         }
     }
 }
