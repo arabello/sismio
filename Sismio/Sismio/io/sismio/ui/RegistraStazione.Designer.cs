19cf4447513386e5bff37b21cb73fb2324165ca7
albertobagnacani <albertobagnacani@gmail.com>
2018-06-07 17:04:34 +0200

    Add RegistraStazione implementation

--- Sismio/Sismio/io/sismio/ui/RegistraStazione.Designer.cs
+++ Sismio/Sismio/io/sismio/ui/RegistraStazione.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.inputNome = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.inputOrigine = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.inputLocazione = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.inputIP = new MaterialSkin.Controls.MaterialSingleLineTextField();
             this.inputPorta = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.btnAnnulla = new MaterialSkin.Controls.MaterialFlatButton();
             this.btnCrea = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnAnnulla = new MaterialSkin.Controls.MaterialFlatButton();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +44,7 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.inputNome, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.inputOrigine, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.inputLocazione, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.inputIP, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.inputPorta, 1, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(30, 18);
@@ -74,23 +74,23 @@
             this.inputNome.Text = "Nome stazione";
             this.inputNome.UseSystemPasswordChar = false;
             // 
-            // inputOrigine
+            // inputLocazione
             // 
-            this.inputOrigine.Depth = 0;
-            this.inputOrigine.Hint = "";
-            this.inputOrigine.Location = new System.Drawing.Point(272, 3);
-            this.inputOrigine.MaxLength = 32767;
-            this.inputOrigine.MouseState = MaterialSkin.MouseState.HOVER;
-            this.inputOrigine.Name = "inputOrigine";
-            this.inputOrigine.PasswordChar = '\0';
-            this.inputOrigine.SelectedText = "";
-            this.inputOrigine.SelectionLength = 0;
-            this.inputOrigine.SelectionStart = 0;
-            this.inputOrigine.Size = new System.Drawing.Size(192, 23);
-            this.inputOrigine.TabIndex = 1;
-            this.inputOrigine.TabStop = false;
-            this.inputOrigine.Text = "Locazione";
-            this.inputOrigine.UseSystemPasswordChar = false;
+            this.inputLocazione.Depth = 0;
+            this.inputLocazione.Hint = "";
+            this.inputLocazione.Location = new System.Drawing.Point(272, 3);
+            this.inputLocazione.MaxLength = 32767;
+            this.inputLocazione.MouseState = MaterialSkin.MouseState.HOVER;
+            this.inputLocazione.Name = "inputLocazione";
+            this.inputLocazione.PasswordChar = '\0';
+            this.inputLocazione.SelectedText = "";
+            this.inputLocazione.SelectionLength = 0;
+            this.inputLocazione.SelectionStart = 0;
+            this.inputLocazione.Size = new System.Drawing.Size(192, 23);
+            this.inputLocazione.TabIndex = 1;
+            this.inputLocazione.TabStop = false;
+            this.inputLocazione.Text = "Locazione";
+            this.inputLocazione.UseSystemPasswordChar = false;
             // 
             // inputIP
             // 
@@ -128,23 +128,6 @@
             this.inputPorta.Text = "Porta";
             this.inputPorta.UseSystemPasswordChar = false;
             // 
-            // btnAnnulla
-            // 
-            this.btnAnnulla.AutoSize = true;
-            this.btnAnnulla.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnAnnulla.Depth = 0;
-            this.btnAnnulla.Icon = null;
-            this.btnAnnulla.Location = new System.Drawing.Point(363, 338);
-            this.btnAnnulla.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.btnAnnulla.MouseState = MaterialSkin.MouseState.HOVER;
-            this.btnAnnulla.Name = "btnAnnulla";
-            this.btnAnnulla.Primary = false;
-            this.btnAnnulla.Size = new System.Drawing.Size(83, 36);
-            this.btnAnnulla.TabIndex = 2;
-            this.btnAnnulla.Text = "annulla";
-            this.btnAnnulla.UseVisualStyleBackColor = true;
-            this.btnAnnulla.Click += new System.EventHandler(this.materialFlatButton1_Click);
-            // 
             // btnCrea
             // 
             this.btnCrea.AutoSize = true;
@@ -161,14 +144,32 @@
             this.btnCrea.TabIndex = 8;
             this.btnCrea.Text = "crea";
             this.btnCrea.UseVisualStyleBackColor = true;
+            this.btnCrea.Click += new System.EventHandler(this.btnCrea_Click);
+            // 
+            // btnAnnulla
+            // 
+            this.btnAnnulla.AutoSize = true;
+            this.btnAnnulla.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAnnulla.Depth = 0;
+            this.btnAnnulla.Icon = null;
+            this.btnAnnulla.Location = new System.Drawing.Point(363, 338);
+            this.btnAnnulla.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnAnnulla.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnAnnulla.Name = "btnAnnulla";
+            this.btnAnnulla.Primary = false;
+            this.btnAnnulla.Size = new System.Drawing.Size(83, 36);
+            this.btnAnnulla.TabIndex = 10;
+            this.btnAnnulla.Text = "annulla";
+            this.btnAnnulla.UseVisualStyleBackColor = true;
+            this.btnAnnulla.Click += new System.EventHandler(this.btnAnnulla_Click);
             // 
             // RegistraStazione
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAnnulla);
             this.Controls.Add(this.btnCrea);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.btnAnnulla);
             this.MaximumSize = new System.Drawing.Size(600, 400);
             this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "RegistraStazione";
@@ -183,10 +184,10 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private MaterialSkin.Controls.MaterialSingleLineTextField inputNome;
-        private MaterialSkin.Controls.MaterialSingleLineTextField inputOrigine;
+        private MaterialSkin.Controls.MaterialSingleLineTextField inputLocazione;
         private MaterialSkin.Controls.MaterialSingleLineTextField inputIP;
         private MaterialSkin.Controls.MaterialSingleLineTextField inputPorta;
-        private MaterialSkin.Controls.MaterialFlatButton btnAnnulla;
         private MaterialSkin.Controls.MaterialRaisedButton btnCrea;
+        private MaterialSkin.Controls.MaterialFlatButton btnAnnulla;
     }
 }
--- Sismio/Sismio/io/sismio/ui/RegistraStazione.cs
+++ Sismio/Sismio/io/sismio/ui/RegistraStazione.cs
@@ -4,22 +4,68 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sismio.io.sismio.stazione;
 
 namespace Sismio.io.sismio.ui
 {
     public partial class RegistraStazione : UserControl
     {
+
+        public IGestioneStazioniController Controller;
+
         public RegistraStazione()
         {
             InitializeComponent();
         }
 
-        private void materialFlatButton1_Click(object sender, EventArgs e)
+        private String validation()
         {
+            if (inputNome.Text == "Nome stazione" || inputNome.Text == "")
+                return "Nome stazione";
+            if (inputLocazione.Text == "Locazione" || inputLocazione.Text == "")
+                return "Locazione";
+            if (inputIP.Text == "Indirizzo IP" || inputIP.Text == "" || ! IPAddress.TryParse(inputIP.Text, out IPAddress ind))
+                return "Indirizzo IP";
+            if (inputPorta.Text == "Porta" || inputPorta.Text == "" || ! int.TryParse(inputPorta.Text, out int number))
+                return "Porta";
+            return null;
+        }
 
+        private void btnCrea_Click(object sender, EventArgs e)
+        {
+            String error = validation();
+            if (error != null)
+            {
+                MessageBox.Show("Il campo " + error + " non è corretto", "Inserimento dati non valido",
+                    MessageBoxButtons.OK);
+                return;
+            }
+
+            Stazione stazione;
+
+            stazione = new Stazione
+            {
+                Nome = inputNome.Text,
+                Locazione = inputLocazione.Text,
+                IndirizzoDiRete = IPAddress.Parse(inputIP.Text),
+                Porta = Int32.Parse(inputPorta.Text),
+                ImprontaChiavePubblica = "AAGGA616625H2AG126G161AG16A1G62" // TODO
+            };
+            
+            bool res = Controller.Registra(stazione);
+            if (res)
+                this.ParentForm.DialogResult = DialogResult.Abort;
+            else
+                this.ParentForm.DialogResult = DialogResult.OK;
+        }
+
+        private void btnAnnulla_Click(object sender, EventArgs e)
+        {
+            this.ParentForm.DialogResult = DialogResult.Cancel;
         }
     }
 }
