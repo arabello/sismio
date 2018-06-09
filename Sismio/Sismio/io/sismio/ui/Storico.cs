3351af0d0d3f9aa4659a978eb1a116018297d17c
Federico Terzi <freddy6896@gmail.com>
2018-06-09 08:52:03 +0200

    Sistemato bug storico

--- Sismio/Sismio/io/sismio/ui/Storico.cs
+++ Sismio/Sismio/io/sismio/ui/Storico.cs
@@ -104,7 +104,7 @@ namespace Sismio.io.sismio.ui
                 DateTime date = UnixTimeStampToDateTime(evento.Timestamp);
                 var version = new string[]
                 {
-                    date.Date.ToString(), evento.Tag, evento.Stazione?.Nome, evento.Messaggio, evento.Priorita.ToString()
+                    date.ToString(), evento.Tag, evento.Stazione?.Nome, evento.Messaggio, evento.Priorita.ToString()
                 };
                 var item = new ListViewItem(version);
                 item.BackColor = Color.AliceBlue;
